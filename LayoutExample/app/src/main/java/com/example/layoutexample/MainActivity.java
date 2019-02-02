3f86bd5385b55a7e7aad7693b8c9fc5f48ba0b97
Joe <sponde210@gmail.com>
2019-02-01 20:49:55 -0500

    add RelativeLayout

---
+++ LayoutExample/app/src/main/java/com/example/layoutexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.layoutexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showLinearLayoutVertical(View view) {
+        startActivity(new Intent(this, LinearLayoutVertical.class));
+    }
+
+    public void showLinearLayoutHorizontal(View view) {
+        startActivity(new Intent(this, LinearLayoutHorizontal.class));
+    }
+
+    public void showTableLayout(View view) {
+        startActivity(new Intent(this, TableLayout.class));
+    }
+
+    public void showGridLayout(View view) {
+        startActivity(new Intent(this, RelativeLayout.class));
+    }
+}
